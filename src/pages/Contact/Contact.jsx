29f0768e9242b953b356245933216aa6c3ffb021
pabloblockchain24 <pablo.arce.p@mail.pucv.cl>
2025-04-12 14:36:39 -0400

    add notification mail

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,34 +1,46 @@
-import { useForm } from "react-hook-form"
-
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 function Contact() {
-  const { handleSubmit, register } = useForm()
+  const { handleSubmit, register } = useForm();
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data)
-
     try {
-      await fetch("https://portfolio-backend-arce.vercel.app/api/contactMail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      })
+      const response = await fetch(
+        "https://portfolio-backend-arce.vercel.app/api/contactMail",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        setMessage("¡Correo enviado correctamente!");
+        setIsError(false);
+        setTimeout(() => setMessage(""), 5000);
+      } else {
+        setMessage("Hubo un error al enviar el correo.");
+        setIsError(true);
+        setTimeout(() => setMessage(""), 5000);
+      }
     } catch (error) {
-      console.error(error)
+      console.error(error);
+      setMessage("Hubo un error al enviar el correo.");
+      setIsError(true);
+      setTimeout(() => setMessage(""), 5000);
     }
-
-  })
-
+  });
 
   return (
-    <section className='contact' id="contact">
-     <h1>Contact</h1>
+    <section className="contact" id="contact">
+      <h1>Contact</h1>
       <div className="contact-card">
         <form onSubmit={onSubmit} className="contact-card-form">
-
           <article className="contact-card-form-article">
-            <input 
+            <input
               type="text"
               {...register("name", { required: true })}
               placeholder="Juan Perez"
@@ -36,7 +48,7 @@ function Contact() {
           </article>
 
           <article className="contact-card-form-article">
-            <input 
+            <input
               type="email"
               {...register("email", { required: true })}
               placeholder="juanperez@gmail.com"
@@ -51,16 +63,24 @@ function Contact() {
             ></textarea>
           </article>
 
-          <button type="submit" className="contact-card-form-button"> ENVIAR </button>
+          <div className="contact-card-form-button-container">
+            <button type="submit" className="contact-card-form-button">
+              {" "}
+              ENVIAR{" "}
+            </button>
+            {message && (
+              <p style={{ color: isError ? "red" : "green" }}>{message}</p>
+            )}
+          </div>
 
           <hr />
-          <footer className='contact-card-form-footer'>
+          <footer className="contact-card-form-footer">
             Mi correo es: parcepaiva@gmail.com
           </footer>
         </form>
       </div>
-    </section >
-  )
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
